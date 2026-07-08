a28b3386c241c59a4385c5f4c46a8c8c83f3bfb2
KANNITHI <6710301041@cdti.ac.th>
2026-07-08 10:31:33 +0700

    finish

--- Week3/task_04_callback.py
+++ Week3/task_04_callback.py
@@ -3,7 +3,7 @@ import asyncio
 from time import ctime
 
 def alert_manager(finished_task):
-    # 
+    # callback function automatically accepts the completed task object as its first argument
     print(f"{ctime()} Callback Triggered! Task output fetched: {finished_task.result()}")
 
 async def download_file():
@@ -13,9 +13,9 @@ async def download_file():
 
 async def main():
     task = asyncio.create_task(download_file())
-    # 
+    # Register a callback function (Do not add parentheses '()' when passing it)
     task.add_done_callback(alert_manager)
     
-    await task # 
+    await task # wait here the what
 
 asyncio.run(main())
